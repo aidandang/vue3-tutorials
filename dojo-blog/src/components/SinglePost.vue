e1a18dc420effedd3b83769e0775e5020e7758cf
Aidan Dang <aidandang@gmail.com>
2021-12-12 17:21:53 -0800

    working on composition API

---
+++ dojo-blog/src/components/SinglePost.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="post">
+		<h3>{{ post.title }}</h3>
+		<p>{{ snippet }}</p>
+	</div>
+</template>
+
+<script>
+import { computed } from '@vue/reactivity';
+export default {
+	name: 'SinglePost',
+	props: ['post'],
+	setup(props) {
+		const snippet = computed(() => {
+			return props.post.body.substring(0, 75) + '....';
+		});
+
+		return { snippet };
+	},
+};
+</script>
+
+<style></style>
---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="home">
+		<h1>Home</h1>
+		<PostList v-if="showPosts" :posts="posts" />
+		<button @click="showPosts = !showPosts">toggle posts</button>
+		<button @click="posts.pop()">delete a post</button>
+	</div>
+</template>
+
+<script>
+import PostList from '../components/PostList.vue';
+import { ref } from 'vue';
+
+export default {
+	name: 'Home',
+	components: { PostList },
+	setup() {
+		const posts = ref([
+			{
+				title: 'Welcome to the blog',
+				body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, reiciendis ullam. Id tempore libero, nisi molestiae ipsa deserunt, eos iure architecto odio voluptatibus accusantium ea amet? Exercitationem saepe magni in.',
+				id: 1,
+			},
+			{
+				title: 'Top 5 CSS tips',
+				body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, reiciendis ullam. Id tempore libero, nisi molestiae ipsa deserunt, eos iure architecto odio voluptatibus accusantium ea amet? Exercitationem saepe magni in.',
+				id: 2,
+			},
+		]);
+
+		const showPosts = ref(true);
+		return { posts, showPosts };
+	},
+};
+</script>
