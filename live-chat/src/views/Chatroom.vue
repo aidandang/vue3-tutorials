8eadef75e1a3939c1c46474050237e73ce324e6c
W Brand Studio 05 <wbrandstudio05@ws-imac.lan>
2021-12-13 17:05:46 -0800

    added live-chat app

---
+++ live-chat/src/views/Chatroom.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="container">
+		<Navbar />
+	</div>
+</template>
+
+<script>
+import { watch } from 'vue';
+
+import Navbar from '../components/Navbar.vue';
+import getUser from '../composables/getUser';
+import { useRouter } from 'vue-router';
+
+export default {
+	name: 'Chatroom',
+	components: { Navbar },
+	setup() {
+		const { user } = getUser();
+		const router = useRouter();
+
+		watch(user, () => {
+			if (!user.value) {
+				router.push({ name: 'Welcome' });
+			}
+		});
+	},
+};
+</script>
+
+<style></style>
