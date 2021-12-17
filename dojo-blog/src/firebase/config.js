70414d41d18331fde6fcbab8e7c6f541e27e087f
W Brand Studio 05 <wbrandstudio05@ws-imac.lan>
2021-12-16 18:02:17 -0800

    working on the project

---
+++ dojo-blog/src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyC7ObDXNPIsPuU5S9QxoVeQ_iMSFTPKRTc',
+	authDomain: 'dojo-blog-df4a9.firebaseapp.com',
+	projectId: 'dojo-blog-df4a9',
+	storageBucket: 'dojo-blog-df4a9.appspot.com',
+	messagingSenderId: '617968244121',
+	appId: '1:617968244121:web:da8c8c4b1c32e167356f08',
+	measurementId: 'G-3K98NDFE9K',
+};
+
+firebase.initializeApp(firebaseConfig);
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, timestamp };
---
+++ muso-ninjas/src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyCb9Apx9XnC_FjOazC8cVkvntB3waCFeAg',
+	authDomain: 'muso-ninjas-21e0d.firebaseapp.com',
+	projectId: 'muso-ninjas-21e0d',
+	storageBucket: 'muso-ninjas-21e0d.appspot.com',
+	messagingSenderId: '221700465421',
+	appId: '1:221700465421:web:53ccb8b30552e530de0644',
+	measurementId: 'G-1VQ8YJFD71',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// setup timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };
