c412952d49dc7fd824a103a29e8f0d942a4f6877
W Brand Studio 05 <wbrandstudio05@ws-imac.lan>
2021-12-29 15:49:09 -0800

    add pawsmatch

---
+++ muso-ninjas/src/views/Pawsmatch.vue
@@ -0,0 +1,16 @@
+<template>
+	<iframe
+		src="http://pawsmatch.local/dogs"
+		title="description"
+		style="min-width: 960px; min-height: 1960px"
+		width="100%"
+	></iframe>
+</template>
+
+<script>
+export default {
+	name: 'Pawsmatch',
+};
+</script>
+
+<style></style>
